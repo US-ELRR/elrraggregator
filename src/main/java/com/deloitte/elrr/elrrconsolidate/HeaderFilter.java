a18dde7f54b33deb7ba12c5d1d9e88bd347d560a
Leven <phleven@deloitte.com>
2024-10-31 14:13:31 -0400

    Add edit switches.

--- src/main/java/com/deloitte/elrr/elrrconsolidate/HeaderFilter.java
+++ src/main/java/com/deloitte/elrr/elrrconsolidate/HeaderFilter.java
@@ -1,27 +1,53 @@
 package com.deloitte.elrr.elrrconsolidate;
 
-import jakarta.servlet.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 @Slf4j
 public class HeaderFilter implements Filter {
+    
+    @Value("${http.header}")
+    private String httpHeader;
+
     @Override
     public
     void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
-        if("https".equalsIgnoreCase(httpServletRequest.getHeader("X-Forwarded-Proto"))) {
+        if (httpHeader != null 
+                && !httpHeader.trim().isEmpty()) {
+            
+            if (httpHeader.equalsIgnoreCase("off")) {
+                chain.doFilter(request, response);
+            } else {
+                if("https".equalsIgnoreCase(httpServletRequest.getHeader("X-Forwarded-Proto"))) {
+                    chain.doFilter(request, response);
+                } else {
+                    log.error("Not a HTTPS request.");
+                    ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Not a HTTPS request.");
+                }
+            }
+            
+        } else {
+            
             chain.doFilter(request, response);
-        }else {
-            log.error("Not a HTTPS request.");
-            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Not a HTTPS request.");
-        }
+            
+        }        
+           
     }
 }
--- src/main/java/com/deloitte/elrr/elrrconsolidate/JSONRequestSizeLimitFilter.java
+++ src/main/java/com/deloitte/elrr/elrrconsolidate/JSONRequestSizeLimitFilter.java
@@ -1,10 +1,8 @@
 package com.deloitte.elrr.elrrconsolidate;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -12,20 +10,26 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class JSONRequestSizeLimitFilter extends OncePerRequestFilter {
     
     @Value("${json.max.size.limit}")
-    private static long maxSizeLimit;
+    private long maxSizeLimit;
+    private long MAX_SIZE_LIMIT = maxSizeLimit;
 
-    private static final long MAX_SIZE_LIMIT = maxSizeLimit;
-    
+    @Value("${mediaType.json}")
+    private String mediaTypeJson;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response, FilterChain filterChain)
+            HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         if (isApplicationJson(request) && request.getContentLengthLong() < MAX_SIZE_LIMIT) {
             filterChain.doFilter(request, response);
@@ -36,8 +40,22 @@ public class JSONRequestSizeLimitFilter extends OncePerRequestFilter {
     }
 
     private boolean isApplicationJson(HttpServletRequest httpRequest) {
-         return (MediaType.APPLICATION_JSON.isCompatibleWith(MediaType
-                 .parseMediaType(httpRequest.getHeader(HttpHeaders.CONTENT_TYPE))));
-    }
+        
+        if (mediaTypeJson != null 
+                && !mediaTypeJson.trim().isEmpty()) {
+            
+            if (mediaTypeJson.equalsIgnoreCase("off")) {
+                return true;
+            } else {
+                return (MediaType.APPLICATION_JSON.isCompatibleWith(MediaType
+                        .parseMediaType(httpRequest.getHeader(HttpHeaders.CONTENT_TYPE))));
+            }
+            
+        } else {
+            
+            return true;
+            
+        } 
+     }
 
 }
