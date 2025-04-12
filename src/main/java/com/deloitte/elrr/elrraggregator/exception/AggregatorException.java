9587d336f674a606a95a7d6d64f7a25fb37e5e66
1Phil <1philleven@gmail,com>
2025-04-12 15:35:45 -0400

    Add serialVersionUID to AggregatorException and PersonNotFoundException.

--- src/main/java/com/deloitte/elrr/elrraggregator/exception/AggregatorException.java
+++ src/main/java/com/deloitte/elrr/elrraggregator/exception/AggregatorException.java
@@ -2,6 +2,8 @@ package com.deloitte.elrr.elrraggregator.exception;
 
 public class AggregatorException extends RuntimeException {
 
+  private static final long serialVersionUID = 1L;
+
   public AggregatorException(String errorMessage) {
     super(errorMessage);
   }
--- src/main/java/com/deloitte/elrr/elrraggregator/exception/PersonNotFoundException.java
+++ src/main/java/com/deloitte/elrr/elrraggregator/exception/PersonNotFoundException.java
@@ -2,6 +2,8 @@ package com.deloitte.elrr.elrraggregator.exception;
 
 public class PersonNotFoundException extends RuntimeException {
 
+  private static final long serialVersionUID = 1L;
+
   public PersonNotFoundException(String errorMessage) {
     super(errorMessage);
   }
