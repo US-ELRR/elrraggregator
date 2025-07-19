ae4a381cb86b3a83f6e739f9c83cb55667435db4
Leven <phleven@deloitte.com>
2025-07-19 15:05:40 -0400

    Re-factor EXtensionsUtil.

--- src/main/java/com/deloitte/elrr/aggregator/utils/ExtensionsUtil.java
+++ src/main/java/com/deloitte/elrr/aggregator/utils/ExtensionsUtil.java
@@ -2,10 +2,7 @@ package com.deloitte.elrr.aggregator.utils;
 
 import java.net.URI;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
@@ -26,21 +23,13 @@ public class ExtensionsUtil {
 
         Extensions extensions = null;
         Map<URI, Object> extensionsMap = new HashMap<>();
-        Set<Entry<URI, Object>> extensionSet = new HashSet<>();
 
         if (context != null) {
 
             extensions = context.getExtensions();
 
             if (extensions != null) {
-
-                extensionSet = extensions.getMap().entrySet();
-
-            }
-
-            // Convert Set to Map
-            for (Map.Entry<URI, Object> entry : extensionSet) {
-                extensionsMap.put(entry.getKey(), entry.getValue());
+                extensionsMap = extensions.getMap();
             }
 
         }
